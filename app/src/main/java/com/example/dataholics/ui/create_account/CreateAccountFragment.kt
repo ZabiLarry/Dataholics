aea99a22122e61b22862ee4bb0888ba11124795b
Vik <viktor.westlund0011@stud.hkr.se>
2020-03-03 13:23:59 +0100

    Create account layout + fragment creation of Login and create account.

---
+++ app/src/main/java/com/example/dataholics/ui/create_account/CreateAccountFragment.kt
@@ -0,0 +1,34 @@
+package com.example.dataholics.ui.create_account
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.dataholics.R
+
+class CreateAccountFragment : Fragment() {
+
+    companion object {
+        fun newInstance() =
+            CreateAccountFragment()
+    }
+
+    private lateinit var viewModel: CreateAccountViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.create_account_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(CreateAccountViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
