968b7631fbd83ddf32d783f305a1feac84158f02
Vik <viktor.westlund0011@stud.hkr.se>
2020-03-13 19:48:43 +0100

    Share application to others
    Simple layout and backend coding for inviting others to use the application via a download link (dummy link).

--- app/src/main/java/com/example/dataholics/ui/share/ShareFragment.kt
+++ app/src/main/java/com/example/dataholics/ui/share/ShareFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -13,6 +14,7 @@ import com.example.dataholics.R
 class ShareFragment : Fragment() {
 
     private lateinit var shareViewModel: ShareViewModel
+    private lateinit var button : Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
