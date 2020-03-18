e4a6ea072b2d9d8e46db55545e8c90bb38b5872a
jonasjoh97 <45165180+jonasjoh97@users.noreply.github.com>
2020-03-18 03:00:02 +0100

    changed some data values

--- app/src/main/java/com/example/dataholics/database/Activity.kt
+++ app/src/main/java/com/example/dataholics/database/Activity.kt
@@ -1,3 +1,3 @@
 package com.example.dataholics.database
 
-class Activity(val activityId: Int, val activityType: String, val company: String, val date: String, val time: Int)
+class Activity(val activityId: Int, val activityType: Int, val company: Int, val date: String, val time: Int)
